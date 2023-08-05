872006b0de69144db27b216c55ddb859dec7b6f2
Luis <eternal_pitta0b@icloud.com>
2023-08-05 16:45:33 -0500

    create service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,18 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [new Ingredient('Garlic', 3)];
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+}
