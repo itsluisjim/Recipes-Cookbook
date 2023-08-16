7ef8af75f41a1e667cde18ed7c2d6699e4e0ee04
Luis <eternal_pitta0b@icloud.com>
2023-08-16 15:11:15 -0500

    implement angular routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output  } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +9,5 @@ export class HeaderComponent {
   
   collapsed = true;
 
-  @Output() toggleTab = new EventEmitter<string>();
 
-
-  onToggle(tab: string) {
-    this.toggleTab.emit(tab)
-  }
 }
