e840c2f95e842898acaf50b2a7e90fe7f8c4d102
Luis <eternal_pitta0b@icloud.com>
2023-08-16 15:15:15 -0500

    load recipe using ID found in URL route

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipes.model';
 import { RecipeService } from '../recipes.service';
 
@@ -7,11 +8,21 @@ import { RecipeService } from '../recipes.service';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
   
-  @Input() recipeInfo: Recipe;
+  recipeInfo: Recipe;
+  id: number
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipeInfo = this.recipeService.getRecipe(this.id);
+      }
+    )
+  }
   
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipeInfo.ingredients);
