d099fa83702cfd62cb6583503dc72db8a924007b
Luis <eternal_pitta0b@icloud.com>
2023-08-16 19:26:49 -0500

    remove old way of loading a selected recipe

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipes.model';
 import { RecipeService } from './recipes.service';
 
 @Component({
@@ -9,17 +8,10 @@ import { RecipeService } from './recipes.service';
   providers: [RecipeService]
 })
 export class RecipeComponent implements OnInit{
-  
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      )
+    
   }
 }
