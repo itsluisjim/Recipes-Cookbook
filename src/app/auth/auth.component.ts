72da94e4d1786f766ed11be0da7371a34035962a
Luis <eternal_pitta0b@icloud.com>
2023-09-10 22:00:13 -0500

    add form functionality for sign ups and sign ins

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+})
+export default class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  submitForm(authForm: NgForm) {
+    if (!authForm.valid) {
+      return;
+    }
+
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    let authObservable: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      authObservable = this.authService.login(email, password);
+    } else {
+      authObservable = this.authService.signup(email, password);
+    }
+
+    authObservable.subscribe({
+        next: (response: any) => {
+          console.log(response);
+          this.isLoading = false;
+        },
+        error: (error: any) => {
+          this.isLoading = false;
+          this.error = error.message;
+        },
+        complete() {},
+    });
+
+    authForm.reset();
+  }
+}
