55e3d26671fca211945215d6e8a26645af0225ff
Luis <eternal_pitta0b@icloud.com>
2024-01-10 16:16:24 -0600

    create new actions

--- src/app/shopping-list/store/shopping-list-reducer.ts
+++ src/app/shopping-list/store/shopping-list-reducer.ts
@@ -33,25 +33,41 @@ export function shoppingListReducer(
         ingredients: [...state.ingredients, ...action.payload],
       };
     case ShoppingListActions.UPDATE_INGREDIENT:
-      const ingredient = state.ingredients[action.payload.index];
+      const ingredient = state.ingredients[state.editedIngredientIndex];
 
       const updatedIngredient = {
         ...ingredient,
-        ...action.payload.ingredient
+        ...action.payload
       }
 
       const updatedIngredients = [...state.ingredients];
-      updatedIngredients[action.payload.index] = updatedIngredient;
+      updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
 
       return {
         ...state,
         ingredients: updatedIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
     case ShoppingListActions.DELETE_INGREDIENT:
       return {
         ...state,
-        ingredients: state.ingredients.filter((ing, index) => index !== action.payload)
+        ingredients: state.ingredients.filter((ing, index) => index !== state.editedIngredientIndex),
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredient: { ...state.ingredients[action.payload]},
+        editedIngredientIndex: action.payload,
+      }
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      }
     default:
       return state;
   }
