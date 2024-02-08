3337756b84be3b9f288998c0bb87977951fdfcae
Luis <eternal_pitta0b@icloud.com>
2024-02-07 21:47:16 -0600

    create action for login & logout

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,15 @@
+import { Action } from "@ngrx/store";
+
+export const LOGIN ='[AUTH] Login'
+export const LOGOUT ='[AUTH] Logout'
+
+export class Login implements Action {
+    readonly type = LOGIN;
+
+    constructor(public payload: {email: string, userId: string, token: string, expiresIn: Date}){}
+}
+export class Logout implements Action {
+    readonly type = LOGOUT;
+}
+
+export type AuthActions = Login | Logout;
